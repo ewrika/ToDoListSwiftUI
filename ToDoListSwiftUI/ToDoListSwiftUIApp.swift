5858d447f6686e7ff061a44609f89e6b661d4a39
ewrika <egorkaomsk_2003@mail.ru>
2024-12-09 03:09:48 +0600

    Initial Commit

---
+++ ToDoListSwiftUI/ToDoListSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListSwiftUIApp.swift
+//  ToDoListSwiftUI
+//
+//  Created by Георгий Борисов on 09.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
