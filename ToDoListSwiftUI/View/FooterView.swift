d1801132b93e3bef34fac4813d82d8c609458e7f
ewrika <egorkaomsk_2003@mail.ru>
2024-12-09 14:40:01 +0600

    Constants+Footer

---
+++ ToDoListSwiftUI/View/FooterView.swift
@@ -0,0 +1,45 @@
+//
+//  FooterView.swift
+//  ToDoListSwiftUI
+//
+//  Created by Георгий Борисов on 09.12.2024.
+//
+
+import SwiftUI
+
+struct FooterView: View {
+    let taskCount: Int
+    @Environment(\.colorScheme) var colorScheme
+    var body: some View {
+        ZStack {
+            (colorScheme == .light ? Color(.lightGray) : Color.figmaGray)
+                .ignoresSafeArea()
+            // Тк не было дизайна для светлой темы, решил поставить такой цвет для поддержки любой темы
+
+            HStack(alignment: .center) {
+                Spacer()
+
+                Text("\(taskCount) Задач")
+                    .font(.Regular12)
+
+                Spacer()
+
+                Button(action: {
+                    
+                }) {
+                    Image(systemName: "square.and.pencil")
+                        .foregroundColor(.figmaYellow)
+                        .font(.system(size: 22))
+                }
+            }
+            .padding(.horizontal, 16)
+        }.ignoresSafeArea()
+        .frame(maxWidth: .infinity, alignment: .center)
+        .frame(height: 50)
+    }
+}
+
+#Preview {
+    FooterView(taskCount: 7)
+        .previewLayout(.sizeThatFits)
+}
